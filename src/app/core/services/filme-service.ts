fb94664a11eb00a44aab07237f33d163b5e686f6
LucasMoraes-Git <wwwgmoraes@gmail.com>
2026-06-23 19:39:55 -0300

    Melhorias

--- src/app/core/services/filme-service.ts
+++ src/app/core/services/filme-service.ts
@@ -7,7 +7,7 @@ import { Filme } from '../model/filme';
   providedIn: 'root',
 })
 export class FilmeService {
-  private apiUrl = 'http://localhost:8090/filmes';
+  private apiUrl = 'http://localhost:8080/filmes';
   private http = inject(HttpClient);
 
   findCartaz(): Observable<Filme[]>{
@@ -19,9 +19,14 @@ export class FilmeService {
     return this.http.get<Filme>(`${this.apiUrl}/buscarNome/${nomeFilme}`);
   }
   
-  //findById(id: Number): Observable<Filme>{
-  //  return null;
-  //}
+  findById(id: Number): Observable<Filme>{
+    return this.http.get<Filme>(`${this.apiUrl}/${id}`);
+  }
+
+  /*
+  O metódo abaixo optei por fazer em sessao-service, através
+  de uma releitura em que substitui id por nome.
+  */ 
 
   //findSessoesByFilmeId(id: Number): Observable<Sessao[]>{
   //  return null;
